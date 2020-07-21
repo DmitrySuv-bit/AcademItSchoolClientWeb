ce071784b0cccbfcd3e1231aa26be517d984e852
suvor <cedjhjd150792>
2020-07-22 00:20:10 +0700

    Задача Temperature converter

--- TemperatureConverter/convert.js
+++ TemperatureConverter/convert.js
@@ -1,27 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
     // noinspection ES6ConvertVarToLetConst
-    var convert_button = document.querySelector(".convert_button");
+    var convertButton = document.querySelector(".convert_button");
     // noinspection ES6ConvertVarToLetConst
-    var reset_button = document.querySelector(".reset_button");
+    var resetButton = document.querySelector(".reset_button");
     // noinspection ES6ConvertVarToLetConst
-    var inlet_temperature = document.querySelector(".entry_field");
+    var inputTemperature = document.querySelector(".entry_field");
     // noinspection ES6ConvertVarToLetConst
-    var in_kelvin = document.querySelector(".under_container input[name=in_Kelvin]");
+    var inKelvin = document.querySelector(".in_kelvin");
     // noinspection ES6ConvertVarToLetConst
-    var in_Fahrenheit = document.querySelector(".under_container input[name=in_Fahrenheit]");
+    var inFahrenheit = document.querySelector(".in_fahrenheit");
 
-    convert_button.addEventListener("click", function () {
-        if (inlet_temperature.value === "") {
+    convertButton.addEventListener("click", function () {
+        if (inputTemperature.value === "") {
             return alert("Enter value");
         }
 
-        in_kelvin.value = +inlet_temperature.value + 273.15;
-        in_Fahrenheit.value = +inlet_temperature.value * 1.8 + 32;
+        inKelvin.innerText = +inputTemperature.value + 273.15;
+        inFahrenheit.innerText = +inputTemperature.value * 1.8 + 32;
     });
 
-    reset_button.addEventListener("click", function () {
-        inlet_temperature.value = "";
-        in_kelvin.value = "";
-        in_Fahrenheit.value = "";
+    resetButton.addEventListener("click", function () {
+        inputTemperature.value = "";
+        inKelvin.innerText = "";
+        inFahrenheit.innerText = "";
     });
 });
