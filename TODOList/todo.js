ebb13efd66aa9c55057d63aaaf67ae36d24bf8fe
suvor <cedjhjd150792>
2020-07-25 23:02:54 +0700

    Задача TODO List

--- TODOList/todo.js
+++ TODOList/todo.js
@@ -1,20 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // noinspection ES6ConvertVarToLetConst
-    var addTodoButton = document.querySelector(".container button[name=add_button]");
-    // noinspection ES6ConvertVarToLetConst
+    var addTodoButton = document.getElementById("add_button");
     var newTodoTextField = document.querySelector(".container input[name=note_entry]");
-    // noinspection ES6ConvertVarToLetConst
     var todoList = document.getElementById("todo_list");
 
     addTodoButton.addEventListener("click", function () {
-        // noinspection ES6ConvertVarToLetConst
         var text = newTodoTextField.value;
 
         if (text === "") {
-            return alert("Введите заметку!!!");
+            alert("Введите заметку!!!");
+            return;
         }
 
-        // noinspection ES6ConvertVarToLetConst
         var todoItem = document.createElement("li");
 
         todoItem.className = "container";
@@ -22,34 +18,32 @@ document.addEventListener("DOMContentLoaded", function () {
         switchToViewMode();
 
         function switchToViewMode() {
-            todoItem.innerHTML = "<input class='text_style' type='text' name='text' readonly/>" +
-                "<div class='align-right'>" +
-                "<button class='button_style' type='button' name='edit_button'>Редактировать</button>" +
-                "<button class='button_style' type='button' name='delete_button'>Удалить</button></div>";
+            todoItem.innerHTML = "<input class='text_style' type='text' name='text' readonly/> " +
+                "<button class='button edit_button' type='button' name='edit_button'>Редактировать</button> " +
+                "<button class='button delete_button' type='button' name='delete_button'>Удалить</button>";
 
             todoItem.querySelector(".text_style").value = text;
 
-            todoItem.querySelector(".container button[name=delete_button]")
+            todoItem.querySelector(".delete_button")
                 .addEventListener("click", function () {
                     todoItem.parentNode.removeChild(todoItem);
                 });
 
-            todoItem.querySelector(".container button[name=edit_button]")
+            todoItem.querySelector(".edit_button")
                 .addEventListener("click", function () {
-                    todoItem.innerHTML = "<input class='text_style' type='text' name='text'/>" +
-                        "<div class='align-right'>" +
-                        "<button class='button_style' type='button' name='save_button'>Сохранить</button>" +
-                        "<button class='button_style' type='button' name='cancel_button'>Отмена</button></div>";
+                    todoItem.innerHTML = "<input class='text_style' type='text' name='text'/> " +
+                        "<button class='button save_button' type='button' name='save_button'>Сохранить</button> " +
+                        "<button class='button cancel_button' type='button' name='cancel_button'>Отмена</button>";
 
                     todoItem.querySelector(".text_style").value = text;
 
-                    todoItem.querySelector(".container button[name=save_button]")
+                    todoItem.querySelector(".save_button")
                         .addEventListener("click", function () {
                             text = todoItem.querySelector(".text_style").value;
                             switchToViewMode();
                         });
 
-                    todoItem.querySelector(".container button[name=cancel_button]")
+                    todoItem.querySelector(".cancel_button")
                         .addEventListener("click", function () {
                             switchToViewMode();
                         });
